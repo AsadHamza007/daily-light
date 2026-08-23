ddaab72abac8d33dd3b08c2a6956ad5cefede522
AsadHamza007 <deploy@codelps.com>
2026-08-23 18:10:00 +0000

    Asma ul Husna (99 names), Masnoon Duain, daily hadees banner, WhatsApp share on every item, animations & polish

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'daily-light-v6';
+const CACHE = 'daily-light-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
