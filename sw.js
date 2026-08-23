664244e3259e4ac21ffaccf6d024538a87316e05
AsadHamza007 <deploy@codelps.com>
2026-08-23 17:45:27 +0000

    Urdu tarjuma audio after each ayah (toggleable), prominent Quran hero card, juz/surah labels, mobile reorder, colour polish

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'daily-light-v4';
+const CACHE = 'daily-light-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
